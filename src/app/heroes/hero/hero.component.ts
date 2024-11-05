4c21bfe49df31e3210357bf2036b110fbe51089e
HarunMeragaSerra <meragaserraharun@gmail.com>
2024-11-05 11:04:59 +0100

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name:string = 'ironman';
+  public age:number = 45;
+
+  get CapitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void{
+    this.name = 'spiderman';
+  }
+  changeAge():void{
+    this.age = 21;
+  }
+
+  resetForm():void{
+    this.age = 45;
+    this.name ="ironman";
+  }
+}
